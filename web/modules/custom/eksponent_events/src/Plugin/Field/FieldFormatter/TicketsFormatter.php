3bcdbe50134182315f60fb90eed91439d518f0f3
Andreas Nielsen <andreas@reload.dk>
2025-09-05 08:56:45 +0200

    Added custom field formatter for the "field_ticket" field. The formatter is implementing the logic regarding the ticket markup:
     - Over 10 tickets - Don't show anything.
     - Between 1 and 10 tickets - Show remaining tickets.
     - 0 tickets - Show "SOLD OUT".
    
    Also added event teaser template and styling. This is a very rough implementation and could use some polish.

---
+++ web/modules/custom/eksponent_events/src/Plugin/Field/FieldFormatter/TicketsFormatter.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\eksponent_events\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\Attribute\FieldFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Plugin implementation of the 'tickets' formatter.
+ */
+#[FieldFormatter(
+  id: 'field_tickets',
+  label: new TranslatableMarkup('Tickets formatter'),
+  field_types: ['integer'],
+)]
+class TicketsFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode): array {
+    $element = [];
+
+    foreach ($items as $delta => $item) {
+      $value = (int)$item->value;
+
+      if ($value === 0) {
+        $element[$delta] = [
+          '#markup' => $this->t('SOLD OUT'),
+          '#type' => 'item',
+        ];
+      } elseif ($value <= 10) {
+        $element[$delta] = [
+          '#markup' => $this->t('@value seats left', ['@value' => $value]),
+          '#type' => 'item',
+        ];
+      }
+    }
+
+    return $element;
+  }
+}
